a8ff6a39df7fb29cf7549b8e91bc4b391f8f217a
Napapron8881 <napapron.bud@cpird.in.th>
2025-09-10 16:33:09 +0700

    Update Config.js

--- Config.js
+++ Config.js
@@ -1,8 +1,9 @@
 // ประกาศค่า URL ที่ใช้เชื่อมต่อกับ Web App
-    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbzwZDlAdAUGYuin9PYLolmzYHQP7-sMP8rnqKIztq63ZTzfKFiqdw-scMhjENWEKxHwOA/exec';
+    const WEB_APP_MEMBER_URL = 'https://script.google.com/macros/s/AKfycbwZ0CX2oG10nAUIsVWdv_bde81nkDRJ_hSu6DhmryuHULRN9FE4fmCbj_C9jNymLFCGZg/exec';
     const WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzftcccXR3nghX_3ogCkmeW-h2gg6FQGczzMg232KBd_f2pCt3vHiCUDsH3GxMsjkExUg/exec';
 //LIFF_ID
     const LIFF_ID = '2008087991-KB2Egmmq'; // LIFF ID Checkin
     const LIFF_HISTORY = '2008087991-noD5366y'; // LIFF ID history
     const LIFF_REGISTER = '2008087991-QRJG4KK6'; // LIFF ID register
+
 
